5bcdf525e1e0c2a55a27dec63c7f347915fe06b6
Jacob-Laptop <its_thrasher@live.ca>
2018-03-27 09:48:34 -0400

    Add a whole whack of Unity stuff:: Player prefab, spawning code, networking beginnings, Player controller and static physics file from last semesters physics course

---
+++ UnityNetworking/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private AudioSource myAs;
+
+    [SerializeField]
+    private float moveSpeed = 10;
+    [SerializeField]
+    private float maxSpeed = 5;
+    [SerializeField]
+    private float rotSpeed = 10;
+    [SerializeField]
+    private float airbourneModifier = 0.2f;
+
+    private bool grounded = true;
+    private bool jumping = false;
+    private bool falling = false;
+    private bool isSprinting = false;
+
+    private float jumpSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        myAs = GetComponent<AudioSource>();
+
+        rb.maxAngularVelocity = (1.5f * Mathf.PI); // Make sure we don't spin too fast
+
+        //jumpSpeed = FindReqJumpSpeed(2.6f); // Figure out what Viy should be to jump 2.6 units all the time
+        jumpSpeed = FIN.FindViForPeak(2.6f);
+    }
+
+
+    // Mostly just move states around in here
+    private void Update()
+    {
+        // If player is spinning but not trying to turn
+        if (Input.GetAxis("Horizontal") == 0 &&
+            rb.angularVelocity.sqrMagnitude > Mathf.Epsilon)
+        {
+            rb.angularDrag = 12; // stop them from spinning
+        }
+        else
+        {
+            rb.angularDrag = 0; // let them rotate freely
+        }
+
+        // keep track of when player is not holding jump
+        if (jumping && Input.GetAxisRaw("Jump") == 0)
+        {
+            jumping = false;
+        }
+
+        // keep track of when the player is falling
+        falling = rb.velocity.y < 0 && Mathf.Abs(rb.velocity.y) > Mathf.Epsilon;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        // figure out if we should be hampering the players controls because they are in the air
+        float airDamp = grounded ? 1 : airbourneModifier;
+        float sprintMod = Input.GetKey(KeyCode.LeftShift) ? 2 : 1;
+
+        // apply the forces by multiplying relevant modifier and input axis
+        rb.AddForce(transform.forward * moveSpeed * airDamp * Input.GetAxisRaw("Vertical") * sprintMod);
+        //rb.AddForce(transform.right * moveSpeed * airDamp * Input.GetAxisRaw("Strafe") * sprintMod);
+        rb.AddTorque(transform.up * rotSpeed * airDamp * Input.GetAxis("Horizontal"));
+
+        Vector2 controllableMovement = new Vector2(rb.velocity.x, rb.velocity.z);
+        if (controllableMovement.sqrMagnitude > (maxSpeed * maxSpeed * sprintMod))
+        {
+            controllableMovement = controllableMovement.normalized * maxSpeed * sprintMod;
+            rb.velocity = new Vector3(controllableMovement.x, rb.velocity.y, controllableMovement.y);
+        }
+
+        // only allow player to jump if they are 
+        //1) on the ground 
+        //2) pressing the jump button 
+        //3) if they are not still holding the jump button from a previous jump
+        if (grounded &&
+            Input.GetAxis("Jump") != 0 &&
+            !jumping)
+        {
+            jumping = true; // player is now holding the jump button
+            rb.AddForce(transform.up * jumpSpeed, ForceMode.VelocityChange); //add jump Viy we found in Start()
+        }
+        else if (Input.GetAxis("Jump") != 0)
+        {
+            grounded = false;
+        }
+
+        // looks less floaty if gravity is more intense during falls
+        // this makes projectiles way harder to calculate though, so turn it off sometimes
+        if (!grounded && falling)
+        {
+            rb.AddForce(2 * Physics.gravity);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // if our groundcheck trigger is on the ground, record that
+        if (other.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // if our groundcheck trigger left the ground, record that
+        if (other.CompareTag("Ground"))
+        {
+            grounded = false;
+        }
+    }
+
+    // re-arrange Vf^2 = Vi^2 + 2ad to find Viy given height
+    private float FindReqJumpSpeed(float height)
+    {
+        return Mathf.Sqrt(-2 * Physics.gravity.y * height);
+    }
+}
---
+++ UnityNetworking/Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour {
+
+    public PlayerController playerPrefab;
+    public GameObject flagPrefab;
+
+    private SphereCollider rangeBubble;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rangeBubble = GetComponent<SphereCollider>();
+        Spawn(playerPrefab.gameObject);
+        Spawn(flagPrefab);
+	}
+
+    private void Spawn(GameObject o)
+    {
+        Vector3 spawnpoint = rangeBubble == null ?
+            transform.position : 
+            ToXZPlane(Random.insideUnitCircle * rangeBubble.radius);
+
+        Instantiate(o,
+            spawnpoint,
+            transform.rotation);
+    }
+
+    private Vector3 ToXZPlane(Vector2 incoming)
+    {
+        Vector3 v = new Vector3(incoming.x, transform.position.y + playerPrefab.transform.localScale.y, incoming.y);
+        return v;
+    }
+}
