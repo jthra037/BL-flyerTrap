ee7448d567af2655bf89169e7bcd8d7790f72ce0
jthra037 <its_thrasher@live.ca>
2018-04-09 12:54:56 -0400

    Stop working on Networking Tutorial Lab

--- UnityNetworking/Assets/Scripts/Health.cs
+++ UnityNetworking/Assets/Scripts/Health.cs
@@ -1,17 +1,28 @@
 ﻿using UnityEngine;
-
-public class Health : MonoBehaviour
+using UnityEngine.UI;
+using UnityEngine.Networking;
+public class Health : NetworkBehaviour
 {
     public const int maxHealth = 100;
+    
+    [SyncVar]
     public int currentHealth = maxHealth;
+    public RectTransform HealthBar;
 
     public void TakeDamage(int amount)
     {
+        if (!isServer)
+        {
+            return;
+        }
+
         currentHealth -= amount;
         if (currentHealth <= 0)
         {
             currentHealth = 0;
             Debug.Log("Dead!");
         }
+
+        HealthBar.sizeDelta = new Vector2(currentHealth, HealthBar.sizeDelta.y);
     }
 }
